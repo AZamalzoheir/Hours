404b69d531032f3694688ec7bdf98aa237642111
AZamalzoheir <A.Zamalzoheir95@gamil.com>
2018-05-06 21:44:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/amalzoheir/hours/HomeActivity.java
@@ -0,0 +1,23 @@
+package com.example.amalzoheir.hours;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Handler homePageHandler = new Handler();
+        homePageHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(HomeActivity.this,RegisterationActivity.class));
+            }
+
+        }, 1000L);
+    }
+}
