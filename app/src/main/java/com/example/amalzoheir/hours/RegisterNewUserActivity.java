5ded8154a0a95071c48aef68e507c8f5f154667f
AZamalzoheir <A.Zamalzoheir95@gamil.com>
2018-05-07 22:32:42 +0200

    adding register form

--- app/src/main/java/com/example/amalzoheir/hours/RegisterNewUserActivity.java
+++ app/src/main/java/com/example/amalzoheir/hours/RegisterNewUserActivity.java
@@ -3,12 +3,11 @@ package com.example.amalzoheir.hours;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterNewUserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_register_new_user);
     }
 }
--- app/src/main/java/com/example/amalzoheir/hours/RegisterationActivity.java
+++ app/src/main/java/com/example/amalzoheir/hours/RegisterationActivity.java
@@ -11,18 +11,24 @@ import android.widget.Button;
 
 public class RegisterationActivity extends AppCompatActivity {
     Button loginButton;
+    Button craeteAccountButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registeration);
         loginButton= (Button)findViewById(R.id.login_btn);
-
+        craeteAccountButton=(Button)findViewById(R.id.create_account_btn);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(RegisterationActivity.this, LoginActivity.class));
             }
         });
-
+        craeteAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterationActivity.this, RegisterNewUserActivity.class));
+            }
+        });
     }
 }
